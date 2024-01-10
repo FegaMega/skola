7f5ee72ebdf4117a594cb4f5f9df9124255760c5
FegaMega <david.smidebrant@gmail.com>
2024-01-10 21:00:39 +0100

    12345

--- lektion2.py
+++ lektion2.py
@@ -2,7 +2,7 @@
 
 
 #lektion 2
-def uppgift8(time_in_mins, speed_in_KMperH):
+def uppgiftLEK2UPP8(time_in_mins, speed_in_KMperH):
     time = time_in_mins
     speed = speed_in_KMperH
     length = speed*(time/60)
@@ -12,7 +12,7 @@ def uppgift8(time_in_mins, speed_in_KMperH):
 
 
 
-def uppgift9(sträcka):
+def uppgiftLEK2UPP9(sträcka):
     sträckaiM = sträcka/100
     print(sträckaiM, "m")
     return sträckaiM
@@ -21,7 +21,7 @@ def uppgift9(sträcka):
 
 
 
-def uppgift10(SEK):
+def uppgiftLEK2UPP10(SEK):
     USD = (SEK/10.17)
     print(USD, "USD")
     return USD
@@ -31,7 +31,7 @@ def uppgift10(SEK):
 
 
 
-def uppgift11(Temp, FToC: bool):
+def uppgiftLEK2UPP11(Temp, FToC: bool):
     if FToC == False: 
         F = (Temp*1.8)+32
         print(F, "Fahrenheit")
@@ -46,5 +46,26 @@ def uppgift11(Temp, FToC: bool):
     else:
         print("FToC är inte ifylld")
         return 2
+def uppgiftLEK3UPP1():
+    text = input("ålder: ")
+    längd = input("längd: ")
+    print(3* text, 3*längd)
+
+def uppgiftLEK3UPP2():
+    print(round(46.987123, 1))
+
+def uppgiftLEK3UPP3():
+    text = int(input("Ålder: "))
+    print(220 - text)
+    return 
+    
+def uppgiftLEK9UPP14():
+    output = [0, 1]
+    for i in range(0, 100):
+        output.append(int(output[i-2] + output[i-1]))
+    print(output)
+#uppgiftLEK3UPP4
+#A)Kvadratens area är 6.25 kvadratcentimeter.
+#B)
 #vilken uppgift som ska köras / kalla funktion
-uppgift11(20, False)
+uppgiftLEK3UPP2()
