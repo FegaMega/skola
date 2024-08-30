d030997c18f878dcbdb520b718d9f21c02370802
FegaMega <david.smidebrant@gmail.com>
2024-08-30 12:22:16 +0200

    fghjk

--- Sten-sax-påse.py
+++ Sten-sax-påse.py
@@ -11,15 +11,24 @@ print("datorns poäng är ", dator_poäng)
 while dator_poäng < 3 and spelare_poäng < 3:
   dator_val = random.choice(val)
   spelare_val=input("\nSten, sax eller påse? ")
+
   print("Datorn valde " + dator_val)
-  if dator_val == spelare_val:
+  # Dålig kod #
+  if dator_val == spelare_val: 
     print("Ni valde samma! Kör igen.")
-  elif (spelare_val == "sten" and dator_val == "Sax") or (spelare_val == "sax" and dator_val == "Påse") or (spelare_val == "påse" and dator_val == "Sten"):
+  
+  elif ( 
+        (spelare_val == "sten" and dator_val == "Sax") or 
+        (spelare_val == "sax" and dator_val == "Påse") or 
+        (spelare_val == "påse" and dator_val == "Sten")
+        ):
     spelare_poäng += 1
     print("Du fick poäng!")
+  
   else:
     dator_poäng += 1
     print("Datorn fick poäng!")
+    
   print("Din poäng är ", spelare_poäng)
   print("Datorns poäng är ", dator_poäng)
 if dator_poäng > spelare_poäng:
